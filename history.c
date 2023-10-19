465fcacdf4630288513d8cf3f35f3335a662a94a
kweku-annan <kwekuannan.dev@gmail.com>
2023-10-18 22:25:59 -0700

    history.c betty conformed

--- history.c
+++ history.c
@@ -31,25 +31,23 @@ return (buf);
  */
 int write_history(info_t *info)
 {
-        ssize_t fd;
-        char *filename = get_history_file(info);
-        list_t *node = NULL;
-
-        if (!filename)
-                return (-1);
-
-        fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
-        free(filename);
-        if (fd == -1)
-                return (-1);
-        for (node = info->history; node; node = node->next)
-        {
-                _putsfd(node->str, fd);
-                _putfd('\n', fd);
-        }
-        _putfd(BUF_FLUSH, fd);
-        close(fd);
-        return (1);
+ssize_t fd;
+char *filename = get_history_file(info);
+list_t *node = NULL;
+if (!filename)
+return (-1);
+fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
+free(filename);
+if (fd == -1)
+return (-1);
+for (node = info->history; node; node = node->next)
+{
+_putsfd(node->str, fd);
+_putfd('\n', fd);
+}
+_putfd(BUF_FLUSH, fd);
+close(fd);
+return (1);
 }
 
 /**
