d2ce8d52cbd3da31400794c58bba989a1fdc8323
kweku-annan <kwekuannan.dev@gmail.com>
2023-10-18 22:32:07 -0700

    Conform to betty style

--- history.c
+++ history.c
@@ -58,45 +58,43 @@ return (1);
  */
 int read_history(info_t *info)
 {
-        int i, last = 0, linecount = 0;
-        ssize_t fd, rdlen, fsize = 0;
-        struct stat st;
-        char *buf = NULL, *filename = get_history_file(info);
-
-        if (!filename)
-                return (0);
-
-        fd = open(filename, O_RDONLY);
-        free(filename);
-        if (fd == -1)
-                return (0);
-        if (!fstat(fd, &st))
-                fsize = st.st_size;
-        if (fsize < 2)
-                return (0);
-        buf = malloc(sizeof(char) * (fsize + 1));
-        if (!buf)
-                return (0);
-        rdlen = read(fd, buf, fsize);
-        buf[fsize] = 0;
-        if (rdlen <= 0)
-                return (free(buf), 0);
-        close(fd);
-        for (i = 0; i < fsize; i++)
-                if (buf[i] == '\n')
-                {
-                        buf[i] = 0;
-                        build_history_list(info, buf + last, linecount++);
-                        last = i + 1;
-                }
-        if (last != i)
-                build_history_list(info, buf + last, linecount++);
-        free(buf);
-        info->histcount = linecount;
-        while (info->histcount-- >= HIST_MAX)
-                delete_node_at_index(&(info->history), 0);
-        renumber_history(info);
-        return (info->histcount);
+int i, last = 0, linecount = 0;
+ssize_t fd, rdlen, fsize = 0;
+struct stat st;
+char *buf = NULL, *filename = get_history_file(info);
+if (!filename)
+return (0);
+fd = open(filename, O_RDONLY);
+free(filename);
+if (fd == -1)
+return (0);
+if (!fstat(fd, &st))
+fsize = st.st_size;
+if (fsize < 2)
+return (0);
+buf = malloc(sizeof(char) * (fsize + 1));
+if (!buf)
+return (0);
+rdlen = read(fd, buf, fsize);
+buf[fsize] = 0;
+if (rdlen <= 0)
+return (free(buf), 0);
+close(fd);
+for (i = 0; i < fsize; i++)
+if (buf[i] == '\n')
+{
+buf[i] = 0;
+build_history_list(info, buf + last, linecount++);
+last = i + 1;
+}
+if (last != i)
+build_history_list(info, buf + last, linecount++);
+free(buf);
+info->histcount = linecount;
+while (info->histcount-- >= HIST_MAX)
+delete_node_at_index(&(info->history), 0);
+renumber_history(info);
+return (info->histcount);
 }
 
 /**
@@ -109,15 +107,13 @@ int read_history(info_t *info)
  */
 int build_history_list(info_t *info, char *buf, int linecount)
 {
-        list_t *node = NULL;
-
-        if (info->history)
-                node = info->history;
-        add_node_end(&node, buf, linecount);
-
-        if (!info->history)
-                info->history = node;
-        return (0);
+list_t *node = NULL;
+if (info->history)
+node = info->history;
+add_node_end(&node, buf, linecount);
+if (!info->history)
+info->history = node;
+return (0);
 }
 
 /**
@@ -128,13 +124,12 @@ int build_history_list(info_t *info, char *buf, int linecount)
  */
 int renumber_history(info_t *info)
 {
-        list_t *node = info->history;
-        int i = 0;
-
-        while (node)
-        {
-                node->num = i++;
-                node = node->next;
-        }
-        return (info->histcount = i);
+list_t *node = info->history;
+int i = 0;
+while (node)
+{
+node->num = i++;
+node = node->next;
+}
+return (info->histcount = i);
 }
